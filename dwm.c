32ab02692a57783c1bb7fe37b30417c4c98ca8ed
Paul Meffle <meffle.paul@me.com>
2019-01-18 15:47:28 +0100

    Use different name strings for selected tags

--- dwm.c
+++ dwm.c
@@ -60,6 +60,7 @@
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeSel }; /* color schemes */
+enum { TagNorm, TagSel }; /* tag arrays */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
@@ -432,7 +433,10 @@ buttonpress(XEvent *e)
 	if (ev->window == selmon->barwin) {
 		i = x = 0;
 		do
-			x += TEXTW(tags[i]);
+			if (m->tagset[m->seltags] & 1 << i)
+				x += TEXTW(tags[i][TagSel]);
+			else
+				x += TEXTW(tags[i][TagNorm]);
 		while (ev->x >= x && ++i < LENGTH(tags));
 		if (i < LENGTH(tags)) {
 			click = ClkTagBar;
@@ -715,9 +719,15 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
-		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+		if (m->tagset[m->seltags] & 1 << i) {
+			w = TEXTW(tags[i][TagSel]);
+			drw_setscheme(drw, scheme[SchemeSel]);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i][TagSel], urg & 1 << i);
+		} else {
+			w = TEXTW(tags[i][TagNorm]);
+			drw_setscheme(drw, scheme[SchemeNorm]);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i][TagNorm], urg & 1 << i);
+		}
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
 				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
